7cae5e7b6a69001c3ce3daad0e46b1d6fee2a5f6
Gordan Saw <ks.saw@appdevteam.work>
2023-06-24 13:14:38 +0800

    Create Movie Gallery iOS Project

---
+++ Movie Gallery iOS/Movie Gallery iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Gallery iOS
+//
+//  Created by Gordan Saw on 6/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
